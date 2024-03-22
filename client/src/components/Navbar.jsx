16f9f21634887f9175ac9c3e5d4aaa5debf2e6a1
Siddharth <jkrish930@gmail.com>
2024-03-22 19:03:13 +0530

    added order link in nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -110,7 +110,14 @@ const Navbar = () => {
         />
     </div>
 
-
+    {auth &&  (
+          <div className="flex flex-col">
+          <Link to="/orders" className="hover:text-black select-none mr-10">
+            My Orders
+          </Link>
+         
+        </div>
+      )}
     {/* <NavList auth={auth} /> */}
 
     <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
